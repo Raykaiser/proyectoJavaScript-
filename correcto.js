ad56c0f3d40f94b4e49051c7dfb972fe774a1503
Programador <luxitox17@gmail.com>
2022-08-29 19:24:27 -0400

    Proyecto javascript

---
+++ correcto.js
@@ -0,0 +1,8 @@
+var perro = prompt ("Toma uno y rasca su cabeza, negro es lo que antes rojo era ¿QUE ES?(Respuesta en Minuscula)");
+var respuesta = "fosforo";
+
+if (perro==respuesta) {
+document.write("Correcto");
+
+};
+
---
+++ index.js
@@ -0,0 +1,10 @@
+var perro = prompt ("Toma uno y rasca su cabeza, negro es lo que antes rojo era ¿QUE ES?(Respuesta en Minuscula)");
+var respuesta = "fosforo";
+
+if (perro==respuesta) {
+document.write("Correcto");
+document.write()
+}else{
+document.write("Incorrecto"); 
+      
+};
